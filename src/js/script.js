3b4fc15116f94bc01a5961cc3459b92abaeeb606
Patryk Pluta <patryk_pluta@interia.pl>
2022-03-05 19:00:04 +0100

    add favorite

--- src/js/script.js
+++ src/js/script.js
@@ -7,38 +7,69 @@
     },
 
     containerOf: {
-      containerBook: '#book-list',
+      containerBooks: '.books-list',
     },
 
     all: {
-      menuBooks: '#book-list > .book',
-    }
+      menuBooks: '.books-list > .book',
+    },
+
+    bookFavorite: {
+      menuImage: '.books-list > .book__image',
+    },
 
   };
 
   const template = {
-    menuBook: Handlebars.complie(document.querySelector(select.templateOf.templateBook).innerHTML),
+    menuBook: Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML),
   };
   
-  class Book {
-    constructor(id, data){
-      const thisBook = this;
-      thisBook.id = id;
-      thisBook.data = data;
-      thisBook.render();
+  class BooksList {
+
+    constructor(){
+      const thisBooksList = this;
+
+      thisBooksList.data = dataSource.books;
+      thisBooksList.getElements();
+      thisBooksList.render();
+      thisBooksList.initActions();
     }
   
+    getElements() {
+      const thisBooksList = this;
+
+      thisBooksList.dom = {};
+      thisBooksList.dom.wrapper = document.querySelector(select.containerOf.containerBooks);
+    
+      
+    }
 
     render() {
-      const thisBook = this;
+      const thisBooksList = this;
 
-      for(let id of dataSource.books) {
-        const generatedHTML = template.menuBook(thisBook.data);
-        thisBook.element = utils.createDOMFromHTML(generatedHTML);
-        const menuContainer = document.querySelector(select.all.menuBooks);
-        menuContainer.appendChild(thisBook.element);
+      for(let book of thisBooksList.data) {
+        const generatedHTML = template.menuBook(book);
+        const elem = utils.createDOMFromHTML(generatedHTML); 
+        thisBooksList.dom.wrapper.appendChild(elem);
+      }
+    }
+
+    initActions() {
+      const thisBooksList = this;
+
+      thisBooksList.favoriteBooks = {};
+      thisBooksList.clickBooks = document.querySelector(select.bookFavorite.menuImage);
+
+      for(let likeBooks of thisBooksList.clickBooks) {
+
+        likeBooks.addEventListener('dbclick', function(event){
+          event.preventDefault();
+          thisBooksList.clickBooks.classList.add('favorite');
+        });
       }
     }
   }
+
+  new BooksList();
 }
   
